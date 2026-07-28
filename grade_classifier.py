96144df378d9d0759212314409052ae13850aaeb
Kuda_Valen <kudamukwasi@gmail.com>
2026-07-28 14:49:00 +0200

    student info

--- grade_classifier.py
+++ grade_classifier.py
@@ -18,35 +18,50 @@
 def get_student_info():
     name = input("Enter Name: ").strip()
     subject1 = input("Enter Subject 1 Name: ").strip()
-    subject1_marks = float(input("Enter Subject 1 Marks: ")).strip()
+    subject1_marks = float(input("Enter Subject 1 Marks: "))
     subject2 = input("Enter Subject 2 Name: ").strip()
-    subject2_marks = float(input("Enter Subject 2 Marks: ")).strip()
+    subject2_marks = float(input("Enter Subject 2 Marks: "))
     subject3 = input("Enter Subject 3 Name: ").strip()
-    subject3_marks = float(input("enter Subject 3 Marks: ")).strip()
+    subject3_marks = float(input("enter Subject 3 Marks: "))
     subjects = [subject1, subject2, subject3]
     subject_marks = [subject1_marks, subject2_marks, subject3_marks]
 
     average_mark = (subject1_marks+subject2_marks+subject3_marks)/3
     grade_letter = get_grade_letter(average_mark)
     pass_status = get_pass_status(average_mark)
+    student = (name, subjects, subject_marks, grade_letter, pass_status)
+    print("\n=== Student Info ===\n")
+    print(f"Student Name: {student[0].title()}")
+    print("Subjects:")
+    i = 0
+    while i < 3:
+        print(f"Subject: {subjects[i]} : {subject_marks[i]} marks")
+        i += 1
+    print(f"Average Mark: {average_mark}")
+    print(f"Grade Letter: {grade_letter}")
+    print(f"Pass Status: {pass_status}")
 
-    def get_grade_letter(average_mark):
-        if average_mark >= 80:
-            grade_letter = 'A'
-        elif average_mark >= 70 and average_mark < 80:
-            grade_letter = 'B'
-        elif average_mark >= 60 and average_mark < 70:
-            grade_letter = 'C'
-        elif average_mark >= 50 and average_mark < 60:
-            grade_letter = 'D'
-        else:
-            grade_letter = 'F'
-        return grade_letter
+def get_grade_letter(average_mark):
+    if average_mark >= 80:
+        grade_letter = 'A'
+    elif average_mark >= 70 and average_mark < 80:
+        grade_letter = 'B'
+    elif average_mark >= 60 and average_mark < 70:
+        grade_letter = 'C'
+    elif average_mark >= 50 and average_mark < 60:
+        grade_letter = 'D'
+    else:
+        grade_letter = 'F'
+    return grade_letter
 
-    def get_pass_status(average_mark):
-        if average_mark >= 50:
-            pass_status = "Pass"
+def get_pass_status(average_mark):
+    if average_mark >= 50:
+        pass_status = "Pass"
 
-        else:
-            pass_status = "Fail"
-        return pass_status
+    else:
+        pass_status = "Fail"
+    return pass_status
+
+if __name__ == "__main__":
+
+    get_student_info()
